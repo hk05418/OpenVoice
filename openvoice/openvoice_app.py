6891bb1ca41bba0d8b6411942c54f7147339676c
hk05418 <hack005418@gmail.com>
2025-04-11 10:24:30 +0800

    修复port bug 不是int, 添加host

--- openvoice/openvoice_app.py
+++ openvoice/openvoice_app.py
@@ -9,7 +9,8 @@ from openvoice.api import BaseSpeakerTTS, ToneColorConverter
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--share", action='store_true', default=False, help="make link public")
-parser.add_argument("--port", default=9096, help="set gradio port")
+parser.add_argument("--host", type=str, default="0.0.0.0", help="host default 0.0.0.0")
+parser.add_argument("--port", type=int, default=9096, help="set gradio port")
 args = parser.parse_args()
 
 en_ckpt_base = 'checkpoints/base_speakers/EN'
@@ -273,4 +274,4 @@ with gr.Blocks(analytics_enabled=False) as demo:
             tts_button.click(predict, [input_text_gr, style_gr, ref_gr, tos_gr], outputs=[out_text_gr, audio_gr, ref_audio_gr])
 
 demo.queue()  
-demo.launch(debug=True, show_api=True, share=args.share, server_port=args.port)
+demo.launch(debug=True, show_api=True, share=args.share, server_port=args.port, server_name=args.host)
